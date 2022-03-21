c4dcab1fa155ace1152c1ed89413897564d9e767
James Keicher <jamesdkeicher@gmail.com>
2022-03-21 15:30:32 -0700

    forgot to commit progress. All tests pass

--- exercise_7/exercise-7.sql
+++ exercise_7/exercise-7.sql
@@ -1,3 +1,28 @@
 -- find all film titles rented out by customers who live in the city of Dundee
 -- Similar to exercise 6 but you need a few more joins to get the city name
 
+SELECT
+    title
+FROM 
+    city
+LEFT JOIN 
+    address
+ON
+    city.city_id = address.city_id
+LEFT JOIN 
+    customer
+ON
+    address.address_id = customer.address_id
+LEFT JOIN 
+    rental
+ON
+    customer.customer_id = rental.customer_id
+LEFT JOIN 
+    inventory
+ON
+    rental.inventory_id = inventory.inventory_id
+LEFT JOIN
+    film
+ON
+    inventory.film_id = film.film_id
+WHERE city.city = 'Dundee'
--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -1,2 +1,32 @@
 -- find all film titles rented out by customers who live in the country of Peru
 -- Similar to exercise 7 with an additional join to get country name
+
+SELECT
+    title
+FROM 
+    country
+LEFT JOIN
+    city
+ON
+    country.country_id = city.country_id
+LEFT JOIN 
+    address
+ON
+    city.city_id = address.city_id
+LEFT JOIN 
+    customer
+ON
+    address.address_id = customer.address_id
+LEFT JOIN 
+    rental
+ON
+    customer.customer_id = rental.customer_id
+LEFT JOIN 
+    inventory
+ON
+    rental.inventory_id = inventory.inventory_id
+LEFT JOIN
+    film
+ON
+    inventory.film_id = film.film_id
+WHERE country.country = 'Peru'
